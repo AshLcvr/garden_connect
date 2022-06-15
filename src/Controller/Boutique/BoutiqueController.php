fa77407a0e595853219ae5a2b7691e3d0eb80ac4
PaulJORET <paul.joret@hotmail.fr>
2022-06-15 11:56:32 +0200

    form modf profil

--- src/Controller/Boutique/BoutiqueController.php
+++ src/Controller/Boutique/BoutiqueController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller\Boutique;
 
+use App\Entity\User;
 use App\Entity\Boutique;
 use App\Form\BoutiqueType;
+use App\Form\EditProfilType;
 use App\Service\UploadImage;
 use App\Entity\ImagesBoutique;
 use App\Security\EmailVerifier;
@@ -177,5 +179,21 @@ class BoutiqueController extends AbstractController
                 'boutique' => $boutique
             ]
         );
+    }
+
+    #[Route('/boutique/edit/profil/{id}', name: 'boutique_edit_profil', methods: ['GET'])]
+    public function editProfil(Request $request, User $user)
+    {
+        $form = $this->createForm(EditProfilType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            return $this->redirectToRoute('boutique_view_profil', ['id'=> $user->getId()], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('front/boutique/profil/edit_profil.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
     }
 }
---
+++ src/Form/EditProfilType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
+class EditProfilType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Prénom'
+            ])
+            ->add('surname', TextType::class, [
+                'label' => 'Nom'
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'E-mail'
+            ])
+            ->add('submit', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
